22af28494d33f5ddc1afe018941ed478e9ff6847
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-07-03 19:28:30 -0700

    Litteral type

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-type Draggable = {
-  drag: () => void;
-};
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
 
-type Resizable = {
-  resize: () => void;
-};
-
-type UIWidgit = Draggable & Resizable;
-
-let textBox: UIWidgit = {
-  drag: () => {},
-  resize: () => {},
-};
+type Metric = 'cm' | 'inch';
